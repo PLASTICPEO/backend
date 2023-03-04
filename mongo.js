8e41e33f5a9c1b71e4c55fb7b184725a028e2bf4
PLASTICPEO <giodavlasheridze88@gmail.com>
2023-03-05 00:14:25 +0400

    Note, connected Mongodb

--- mongo.js
+++ mongo.js
@@ -1,31 +1,41 @@
 const mongoose = require("mongoose");
 
+if (process.argv.length < 3) {
+  console.log("give password as argument");
+  process.exit(1);
+}
+
+const password = process.argv[2];
+
+const url = `mongodb+srv://fullstack:${password}@cluster0.o1opl.mongodb.net/?retryWrites=true&w=majority`;
+
 mongoose.set("strictQuery", false);
-
-const url = process.env.MONGODB_URI;
-
-console.log("connecting to", url);
-
-mongoose
-  .connect(url)
-  .then((result) => {
-    console.log("connected to MongoDB");
-  })
-  .catch((error) => {
-    console.log("error connecting to MongoDB:", error.message);
-  });
+mongoose.connect(url);
 
 const noteSchema = new mongoose.Schema({
   content: String,
+  date: Date,
   important: Boolean,
 });
 
-noteSchema.set("toJSON", {
-  transform: (document, returnedObject) => {
-    returnedObject.id = returnedObject._id.toString();
-    delete returnedObject._id;
-    delete returnedObject.__v;
-  },
+const Note = mongoose.model("Note", noteSchema);
+
+const note = new Note({
+  content: "Mongoose makes things easy",
+  date: new Date(),
+  important: true,
 });
 
-module.exports = mongoose.model("Note", noteSchema);
+/*
+note.save().then(result => {
+  console.log('note saved!')
+  mongoose.connection.close()
+})
+*/
+
+Note.find({}).then((result) => {
+  result.forEach((note) => {
+    console.log(note);
+  });
+  mongoose.connection.close();
+});
