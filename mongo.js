4d1a20a6fe0a20f116c1d13a331f672442dac65c
PLASTICPEO <giodavlasheridze88@gmail.com>
2023-03-04 16:37:03 +0400

    Note, connect Mongodb

--- mongo.js
+++ mongo.js
@@ -20,20 +20,14 @@ const noteSchema = new mongoose.Schema({
 
 const Note = mongoose.model("Note", noteSchema);
 
-const note = new Note({
-  content: "Mongoose makes things easy",
-  date: new Date(),
-  important: true,
-});
-
-note.save().then((result) => {
-  console.log("note saved!");
-  mongoose.connection.close();
-});
-
-// Note.find({}).then((result) => {
-//   result.forEach((note) => {
-//     console.log(note);
-//   });
+// note.save().then((result) => {
+//   console.log("note saved!");
 //   mongoose.connection.close();
 // });
+
+Note.find({}).then((result) => {
+  result.forEach((note) => {
+    console.log(note);
+  });
+  mongoose.connection.close();
+});
